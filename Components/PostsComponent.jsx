adfcc13055a7347009f2bc0a6573515191acb3c0
BrockSpacek <brockspacek1@gmail.com>
2025-05-06 10:21:07 -0700

    Break

--- Components/PostsComponent.jsx
+++ Components/PostsComponent.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 export default function PostsComponent() {
   return (
     <View>
+      <StoriesComponent />
       <Text style={{color: "white"}}>PostsComponent</Text>
     </View>
   )
--- Components/StoriesComponent.jsx
+++ Components/StoriesComponent.jsx
@@ -1,12 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import profileImage from "../assets/kratos.jpg"
 
 export default function StoriesComponent() {
   return (
-    <View>
-      <Text>StoriesComponent</Text>
-    </View>
+    // We are creating a carousel by adding horizontal to your scroll view component
+    <ScrollView horizontal>
+      
+     {/* Container that holds our stories*/}
+
+      <View style={styles.container}>
+        {/* Gradiant Border Around our image */}
+        <LinearGradiant colors={["#feda75", "#fede1e", "#d62976", "#962fbf", "4f5bd5"]}>
+
+          {/* This view will be our image wrapper*/}
+        
+        <View>
+          {/* Profile Image */}
+          <Image style={styles.image} source={profileImage}/>
+        </View>
+        
+        </LinearGradiant>
+      </View>
+    </ScrollView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    margin: 10
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 50
+  }
+})
