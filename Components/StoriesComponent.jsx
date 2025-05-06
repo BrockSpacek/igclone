ba7e333d208b148d3479661ea983e7754dae38ea
BrockSpacek <brockspacek1@gmail.com>
2025-05-06 10:34:52 -0700

    Stories

--- Components/StoriesComponent.jsx
+++ Components/StoriesComponent.jsx
@@ -1,6 +1,7 @@
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import profileImage from "../assets/kratos.jpg"
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function StoriesComponent() {
   return (
@@ -11,16 +12,77 @@ export default function StoriesComponent() {
 
       <View style={styles.container}>
         {/* Gradiant Border Around our image */}
-        <LinearGradiant colors={["#feda75", "#fede1e", "#d62976", "#962fbf", "4f5bd5"]}>
+        <LinearGradient style={styles.linearGradiant} colors={["#feda75", "#fede1e", "#d62976", "#962fbf", "4f5bd5"]}>
 
           {/* This view will be our image wrapper*/}
         
-        <View>
+        <View style={styles.imageWrapper}>
           {/* Profile Image */}
           <Image style={styles.image} source={profileImage}/>
         </View>
         
-        </LinearGradiant>
+        </LinearGradient>
+        <Text style={styles.labelText}> Kratos </Text>
+      </View>
+
+      <View style={styles.container}>
+        {/* Gradiant Border Around our image */}
+        <LinearGradient style={styles.linearGradiant} colors={["#feda75", "#fede1e", "#d62976", "#962fbf", "4f5bd5"]}>
+
+          {/* This view will be our image wrapper*/}
+        
+        <View style={styles.imageWrapper}>
+          {/* Profile Image */}
+          <Image style={styles.image} source={profileImage}/>
+        </View>
+        
+        </LinearGradient>
+        <Text style={styles.labelText}> Kratos </Text>
+      </View>
+
+      <View style={styles.container}>
+        {/* Gradiant Border Around our image */}
+        <LinearGradient style={styles.linearGradiant} colors={["#feda75", "#fede1e", "#d62976", "#962fbf", "4f5bd5"]}>
+
+          {/* This view will be our image wrapper*/}
+        
+        <View style={styles.imageWrapper}>
+          {/* Profile Image */}
+          <Image style={styles.image} source={profileImage}/>
+        </View>
+        
+        </LinearGradient>
+        <Text style={styles.labelText}> Kratos </Text>
+      </View>
+
+      <View style={styles.container}>
+        {/* Gradiant Border Around our image */}
+        <LinearGradient style={styles.linearGradiant} colors={["#feda75", "#fede1e", "#d62976", "#962fbf", "4f5bd5"]}>
+
+          {/* This view will be our image wrapper*/}
+        
+        <View style={styles.imageWrapper}>
+          {/* Profile Image */}
+          <Image style={styles.image} source={profileImage}/>
+        </View>
+        
+        </LinearGradient>
+        <Text style={styles.labelText}> Kratos </Text>
+      </View>
+
+      <View style={styles.container}>
+        {/* Gradiant Border Around our image */}
+        <LinearGradient style={styles.linearGradiant} colors={["#feda75", "#fede1e", "#d62976", "#962fbf", "4f5bd5"]}>
+
+          {/* This view will be our image wrapper*/}
+        
+        <View style={styles.imageWrapper}>
+          {/* Profile Image */}
+          <Image style={styles.image} source={profileImage}/>
+        </View>
+        
+        </LinearGradient>
+        <Text style={styles.labelText}> Kratos </Text>
       </View>
     </ScrollView>
   )
@@ -37,5 +99,26 @@ const styles = StyleSheet.create({
     width: 80,
     height: 80,
     borderRadius: 50
+  },
+  imageWrapper: {
+    width: 87,
+    height: 87,
+    borderRadius: 50,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  linearGradiant: {
+    width: 94,
+    height: 94,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  labelText: {
+    color: "white",
+    fontSize: 15,
+    marginTop: 5
   }
+
 })
