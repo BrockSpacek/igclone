5f35308813431999dcbf61677f793e9a4cad5728
BrockSpacek <brockspacek1@gmail.com>
2025-05-07 09:50:44 -0700

    Posts part 1

--- Components/PostsComponent.jsx
+++ Components/PostsComponent.jsx
@@ -1,7 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import Kratos from "../assets/Kratos.jpg"
+import Pikachu from "../assets/Pikachu-removebg-preview.png"
 
 export default function PostsComponent() {
+  const [posts, setPosts] = useState([
+    {
+      profilePicture: Kratos,
+      userName: "GodOfWar",
+      postImage: Pikachu,
+      numOfLikes: "103k",
+      description: "Felt cute, might delete later",
+      comments: [
+        {
+          userWhoCommented: "Caterpop_89",
+          userComment: "Das cool!"
+        }
+      ]
+    }
+  ])
   return (
     <View>
       <StoriesComponent />
